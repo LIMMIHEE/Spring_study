23e5595ddfe540896b53eaf22ac821b56d67f09a
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-03-27 15:59:31 +0900

    0327

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,12 +1,29 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 	@RequestMapping("/hello")
 	public String hello() {
-		return "<h1>Hello</h1>";
+		return "hello"; //html 파일 부름
+	}
+	
+	@RequestMapping(value="/bye", method=RequestMethod.GET)
+	public String bye() {
+		return "<h1>bye</h1>";
+	}
+	
+	@RequestMapping(value="/POST", method= {RequestMethod.POST,RequestMethod.GET})
+	public String POST() {
+		return "<h1>bye</h1>";
+	}
+	
+	@PostMapping("post2")
+	public String post2(){
+		return "<h1>TEST</h1><hr>";
 	}
 }
