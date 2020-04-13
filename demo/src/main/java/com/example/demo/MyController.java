632a8062aaddb0f1f2172658e698a08dca38eb32
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-04-13 15:20:49 +0900

    0413 1교시

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,4 +28,14 @@ public class MyController {
 	public String post2(){
 		return "<h1>TEST</h1><hr>";
 	}
+	
+	@GetMapping("/model")
+	public String model(Model m) { // 파라미터에 Model 타입 값 받음
+	// addAttribute(String, Object)
+		m.addAttribute("title", "Hello World 1234"); // 뷰(템플릿) 내부에 필요한 내용 채우기
+		m.addAttribute("content", "Content 1234");
+		
+		return "with_model"; // 역시 템플릿 이름 리턴
+	}
+
 }
